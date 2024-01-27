f50ca72ad9e09df273e74db47c66b87cd36d101c
Hüsnü Mert POLAT <husnumertpolatt@gmail.com>
2024-01-28 02:56:55 +0300

    create ws project

---
+++ src/main/java/com/cryptowallet/wscryptowallet/WscryptowalletApplication.java
@@ -0,0 +1,13 @@
+package com.cryptowallet.wscryptowallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WscryptowalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WscryptowalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptowallet/wscryptowallet/WscryptowalletApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cryptowallet.wscryptowallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WscryptowalletApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+	}
+
+
+
+}
